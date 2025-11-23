32e8532a2080032898585966e371f44a19c0bdfe
Elangovan1350 <elangovan2019miss@gmail.com>
2025-11-23 21:38:47 +0530

    change11

--- src/index.ts
+++ src/index.ts
@@ -3,40 +3,15 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
-
-const libsql = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN,
-});
 
 const adapter = new PrismaLibSql({
   url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
-const prisma = new PrismaClient({ adapter }).$extends({
-  query: {
-    $allModels: {
-      async $allOperations({ operation, args, query }) {
-        const result = await query(args);
-
-        if (["create", "update", "delete"].includes(operation)) {
-          await libsql.sync();
-        }
-
-        return result;
-      },
-    },
-  },
-});
+const prisma = new PrismaClient({ adapter });
 
 const app = new Hono();
-
-app.use("*", async (c, next) => {
-  await libsql.sync(); // sync before handling request
-  await next();
-});
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
